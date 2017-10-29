7eb9a024fe1a905d963601ed24f52e6effb73083
Mykyta Protsenko <mykyta@protsenko.com>
2017-10-28 23:00:51 -0700

    Commented out demo 3

--- terraform3/s3.tf
+++ terraform3/s3.tf
@@ -1,16 +1,16 @@
-terraform {
-  backend "s3" {
-    bucket = "mprotsenko-demo-terraform-state"
-    key = "demo3.tfstate"
-    region = "us-west-1"
-  }
-}
-
-provider "aws" {
-  region = "us-west-1"
-}
-
-module "s3" {
-  source = "s3_module"
-  bucket_name = "demo3-mprotsenko-bucket"
-}
+//terraform {
+//  backend "s3" {
+//    bucket = "mprotsenko-demo-terraform-state"
+//    key = "demo3.tfstate"
+//    region = "us-west-1"
+//  }
+//}
+//
+//provider "aws" {
+//  region = "us-west-1"
+//}
+//
+//module "s3" {
+//  source = "github.com/mykyta-protsenko/tf-demo//terraform3//s3_module"
+//  bucket_name = "demo3-mprotsenko-bucket"
+//}
